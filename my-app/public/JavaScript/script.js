75641e50682349f17aa74a496c160bc18a3e6776
gabrielnalmeida <84048406+gabrielnalmeida@users.noreply.github.com>
2022-12-03 04:39:31 -0300

    Alterações No Slider Agora Está Mais Facil Fazer Manutenção

--- my-app/public/JavaScript/script.js
+++ my-app/public/JavaScript/script.js
@@ -1,29 +1,15 @@
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  breakpoints:{
-      0:{
-          slidesPerView: 1,
-      },
-      520:{
-          slidesPerView: 2,
-      },
-      950:{
-          slidesPerView: 3,
-      },
-  }
-});
+$(document).ready(function() {
+  $('#autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      item:3,
+      auto:true,
+      loop:true,
+      speed: 600,
+      pauseOnHover: true,
+      controls: false,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  }); 
+})
