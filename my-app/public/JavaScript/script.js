fbea8bf08e3dff0da426b45a677643e1e3f39262
gabrielnalmeida <84048406+gabrielnalmeida@users.noreply.github.com>
2022-12-04 18:57:11 -0300

    Mais Alterações no Carrossel
    Agora usando Swiper.js ao invés do lightslider

--- my-app/public/JavaScript/script.js
+++ my-app/public/JavaScript/script.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
-  $('#autoWidth').lightSlider({
-      autoWidth:true,
-      loop:true,
-      item:3,
-      auto:true,
-      loop:true,
-      speed: 600,
-      pauseOnHover: true,
-      controls: false,
-      onSliderLoad: function() {
-          $('#autoWidth').removeClass('cS-hidden');
-      } 
-  }); 
-})
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
